ba56b214373d7663ac4769da0082a863cf6fdbad
namanchoudhary7 <naman.c2005@gmail.com>
2026-06-05 20:34:46 +0530

    message_history

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,10 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
 os.environ['GOOGLE_API_KEY'] = os.getenv('GOOGLE_API_KEY')
 
 
@@ -10,10 +13,20 @@ llm = ChatGoogleGenerativeAI(
     temperature = 1.1,
 )
 
+sys_msg = SystemMessage("You are a helpful assistant")
+messages = [sys_msg]
+
 while True:
     user_input = str(input('User:')).lower()
+
     if user_input=='bye':
         print('Have a nice day! bye')
         break
-    response = llm.invoke(user_input)
+
+    messages.append(HumanMessage(user_input))
+
+    response = llm.invoke(messages)
+
+    messages.append(response)
+
     print('LLM:' + response.content)
